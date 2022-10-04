51a146127a01ffab3b130ebf1884ecfb11b39f29
miyoung-park <qkraldud1529@gmail.com>
2022-10-04 16:08:27 +0900

    connect component test

--- docs/database/create/db_table_init.sql
+++ docs/database/create/db_table_init.sql
@@ -1,24 +1,49 @@
-CREATE TABLE T_USER (
-        user_id	VARCHAR(300)	NOT NULL,
-        passwd	VARCHAR(300)	NOT NULL,
-        user_name	VARCHAR(300)	NOT NULL,
-        email	VARCHAR(300)	NOT NULL,
-        birth	DATE	NULL,
-        phone_number	VARCHAR(300)	NOT NULL,
-        nickname	VARCHAR(300)	NOT NULL,
-        class_name	VARCHAR(300)	NULL,
-        is_del	CHAR(1)	NOT NULL	DEFAULT 'Y'	,
-        grade	VARCHAR(300)	NOT NULL    DEFAULT 'user'
-);
+#사용자
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+    user_idx      INT(11)	        NOT NULL AUTO_INCREMENT	COMMENT '회원인덱스' PRIMARY KEY ,
+    user_id	    VARCHAR(30) 	NOT NULL COMMENT '회원 아이디' UNIQUE KEY,
+    user_pw	    VARCHAR(100)	NOT NULL COMMENT '회원 비밀번호',
+    kdg_idx       INT(11)	        NOT NULL COMMENT '유치원 인덱스',
+    user_name	    VARCHAR(100)	NOT NULL COMMENT '회원 이름',
+    email	        VARCHAR(100)	NOT NULL COMMENT '회원 이메일',
+    birthday	    VARCHAR(14)	    NULL	 COMMENT '회원 생년월일',
+    phone_number	VARCHAR(20)	    NOT NULL COMMENT '회원 전화번호',
+    is_del	    CHAR(1)	        NOT NULL	DEFAULT 'N'	COMMENT '탈퇴유무: (Y/N)',
+    register_time CHAR(14)        NULL DEFAULT NULL COMMENT '등록일자',
+    update_time   CHAR(14)        NULL DEFAULT NULL COMMENT '수정일자'
+    ) AUTO_INCREMENT=1;
 
+#관리자
+DROP TABLE IF EXISTS `t_admin`;
+CREATE TABLE `t_admin` (
+    admin_idx     INT(11)	        NOT NULL AUTO_INCREMENT	COMMENT '관리자인덱스' PRIMARY KEY ,
+    admin_id	  VARCHAR(30) 	    NOT NULL COMMENT '관리자 아이디' UNIQUE KEY,
+    admin_pw	  VARCHAR(100)	    NOT NULL COMMENT '관리자 비밀번호',
+    admin_name	  VARCHAR(100)	    NOT NULL COMMENT '관리자명',
+    admin_email	  VARCHAR(100)	    NOT NULL COMMENT '관리자 이메일',
+    admin_tell	  VARCHAR(20)	    NOT NULL COMMENT '관리자 전화번호',
+    admin_role    CHAR(3)           NOT NULL COMMENT 'SUP:슈퍼관리자, KDG:유치원관리자',
+    kdg_idx       INT(11)	        NULL COMMENT '유치원 인덱스',
+    is_del	      CHAR(1)	        NOT NULL DEFAULT 'N'	COMMENT '탈퇴유무: (Y/N)',
+    register_time CHAR(14)          NULL DEFAULT NULL COMMENT '등록일자',
+    update_time   CHAR(14)          NULL DEFAULT NULL COMMENT '수정일자'
 
-COMMENT ON TABLE T_USER IS '회원';
-COMMENT ON TABLE T_USER.passwd IS '비밀번호';
-COMMENT ON TABLE T_USER.user_name IS '회원이름';
-COMMENT ON TABLE T_USER.email IS '이메일';
-COMMENT ON TABLE T_USER.birth IS '생년월일';
-COMMENT ON TABLE T_USER.phone_number IS '회원전화번호';
-COMMENT ON TABLE T_USER.nickname IS '회원닉네임';
-COMMENT ON TABLE T_USER.class_name IS '학급명';
-COMMENT ON TABLE T_USER.is_del IS '탈퇴:Y/ 회원:N';
-COMMENT ON TABLE T_USER.grade IS '견주:user/업주:school';
+    ) AUTO_INCREMENT=1;
+
+# 유치원
+DROP TABLE IF EXISTS `t_kindergarten`;
+CREATE TABLE `t_kindergarten` (
+    kdg_idx            INT(11)	    NOT NULL AUTO_INCREMENT	COMMENT '유치원인덱스' PRIMARY KEY ,
+    kdg_name	         VARCHAR(100)	NOT NULL COMMENT '유치원명',
+    kdg_address        VARCHAR(100)   NOT NULL COMMENT '유치원 주소',
+    kdg_address_detail VARCHAR(100)	NOT NULL COMMENT '유치원 주소 상세',
+    kdg_tell           VARCHAR(20)	NULL COMMENT '유치원 전화번호',
+    kdg_points	     TEXT           NULL DEFAULT NULL COMMENT '유치원 위치 ',
+    kdg_open_hour      VARCHAR(50)	NOT NULL COMMENT '유치원 오픈시간',
+    kdg_info           TEXT           NULL DEFAULT NULL COMMENT '유치원 정보',
+    service_idx        INT(11)        NULL DEFAULT NULL COMMENT '제공서비스 인덱스',
+    is_del	         CHAR(1)	    NOT NULL	DEFAULT 'N'	COMMENT '삭제유무: (Y/N)',
+    register_time      CHAR(14)       NULL DEFAULT NULL COMMENT '등록일자',
+    update_time        CHAR(14)       NULL DEFAULT NULL COMMENT '수정일자'
+    )  AUTO_INCREMENT=1;
---
+++ server-side/user/src/main/java/com/mi/dang/user/domain/account/AccountController.java
@@ -0,0 +1,14 @@
+package com.mi.dang.user.domain.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 유저 account
+ */
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+
+
+}
